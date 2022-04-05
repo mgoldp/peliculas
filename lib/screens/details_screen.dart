6a67537dd0b91eaa9c82b76ecb1a1ba6b2091953
mgoldp <mgoldp2013@gmail.com>
2022-04-05 12:32:36 -0400

    MovideDetails - Slivers - SliverAppBar

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -3,10 +3,46 @@ import 'package:flutter/material.dart';
 class DeatilsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // TODO: Cambiar luego por una instancia de movie
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+
     return Scaffold(
-      body: Container(
-        child: const Center(
-          child: Text('Details Screen'),
+      body: CustomScrollView(
+        slivers: [
+          _CustomAppBar(),
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: EdgeInsets.all(0),
+        title: Container(
+          color: Colors.black12,
+          child: Text(
+            'movie.title',
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+        ),
+        background: FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/500x300'),
+          fit: BoxFit.cover,
         ),
       ),
     );
