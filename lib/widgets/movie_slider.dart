dc11c34561292af404a6569e171cf5bf610332e3
mgoldp <mgoldp2013@gmail.com>
2022-04-05 01:17:10 -0400

    Finalizar el MovieSlider

--- lib/widgets/movie_slider.dart
+++ lib/widgets/movie_slider.dart
@@ -5,8 +5,7 @@ class MovieSlider extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: 240,
-      color: Colors.red,
+      height: 260,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -16,6 +15,9 @@ class MovieSlider extends StatelessWidget {
               'Populares',
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
+          ),
+          const SizedBox(
+            height: 5,
           ),
           Expanded(
             child: ListView.builder(
@@ -36,8 +38,34 @@ class _MoviePoster extends StatelessWidget {
     return Container(
       width: 130,
       height: 190,
-      color: Colors.green,
-      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        children: [
+          GestureDetector(
+            onTap: (() => Navigator.pushNamed(context, 'details',
+                arguments: 'movie-instance')),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: const FadeInImage(
+                placeholder: AssetImage('assets/no-image.jpj'),
+                image: NetworkImage('https://via.placeholder.com/300x400'),
+                width: 130,
+                height: 190,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 7,
+          ),
+          const Text(
+            'Star Wars: El retorno del nuevo Jedi',
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
     );
   }
 }
