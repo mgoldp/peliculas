54bf4cdd41e80b4a69a03c03a21f9825c670b1a9
mgoldp <mgoldp2013@gmail.com>
2022-04-09 23:02:34 -0400

    Mapeo de peticion HTTP

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:peliculas/models/models.dart';
 
 class MoviesProvider extends ChangeNotifier {
   final String _baseUrl = 'api.themoviedb.org';
@@ -8,21 +9,14 @@ class MoviesProvider extends ChangeNotifier {
   final String _language = 'es-ES';
 
   MoviesProvider() {
-    print('MoviesProvider Inicializado');
     getOnDisplayMovies();
   }
 
   getOnDisplayMovies() async {
     var url = Uri.https(_baseUrl, '3/movie/now_playing',
         {'api_key': _apiKey, 'language': _language, 'page': '1'});
-    print(url);
-    // Await the http get response, then decode the json-formatted response.
+
     final response = await http.get(url);
-
-    print(response.body);
-
-    final Map<String, dynamic> decodedData = json.decode(response.body);
-
-    print(decodedData);
+    final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
   }
 }
